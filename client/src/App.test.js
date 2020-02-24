1c65e15c5a6a5d17f75281173bf03a95763ab13e
Trevor Thompson <trevthom18@gmail.com>
2020-02-23 21:22:50 -0500

    Pushing up most recent code

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import NavBar from "./components/NavBar";
+import { render } from "@testing-library/react";
 import PlayerCard from "./components/PlayerCard";
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  ReactDOM.render(<App />);
 });
 
-xit('NavBar renders without crashing', () => {
+xtest("search for string Marta", () => {
+   const {getByText} = ReactDOM.render(<App />);
+
+   getByText(/marta/i);
+});
+
+xdescribe('NavBar renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<NavBar/>, div);
   ReactDOM.unmountComponentAtNode(div);
 })
 
-xit('A PlayerCard manages to render successfully without crashing the application', () => {
+xdescribe('A PlayerCard manages to render successfully without crashing the application', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PlayerCard/>, div);
   ReactDOM.unmountComponentAtNode(div);
